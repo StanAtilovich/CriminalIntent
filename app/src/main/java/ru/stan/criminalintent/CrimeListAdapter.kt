39e004eb3ce02a6b590937a4d426756a5e6223e9
StanAtilovich <alano92@yandex.ru>
2023-12-15 14:10:29 +0800

    12 Working

--- app/src/main/java/ru/stan/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/ru/stan/criminalintent/CrimeListAdapter.kt
@@ -6,89 +6,42 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import ru.stan.criminalintent.databinding.ListItemCrimeBinding
-import ru.stan.criminalintent.databinding.ListItemSeriousCrimeBinding
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 
 class CrimeHolder(
-    val binding: ListItemCrimeBinding
+    private val binding: ListItemCrimeBinding
+
 ) : RecyclerView.ViewHolder(binding.root) {
+
     fun bind(crime: Crime) {
         binding.crimeTitle.text = crime.title
-        binding.crimeDate.text = crime.data.toString()
+        binding.crimeDate.text = crime.date.toString()
 
         binding.root.setOnClickListener {
             Toast.makeText(
                 binding.root.context,
-                "${crime.title} clicked",
+                "${crime.title} clicked!",
                 Toast.LENGTH_SHORT
             ).show()
         }
-    }
-}
 
-class SeriousCrimeHolder(
-    val binding: ListItemSeriousCrimeBinding
-) : RecyclerView.ViewHolder(binding.root) {
-
-    private val dataFormat =SimpleDateFormat("EEEE, MMMM d, yyyy", Locale.getDefault())
-
-    fun bind(crime: Crime) {
-        binding.crimeTitle.text = crime.title
-        binding.crimeDate.text = dataFormat.format(crime.data)
-        binding.contactPoliceButton.setOnClickListener {
-            Toast.makeText(
-                binding.root.context,
-                "${crime.title} contact police!",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-        binding.root.setOnClickListener {
-            Toast.makeText(
-                binding.root.context,
-                "${crime.title} clicked",
-                Toast.LENGTH_SHORT
-            ).show()
+        // If the crime is solved, make the crimeSolved ImageView visible; else make it gone
+        binding.crimeSolved.visibility = if (crime.isSolved) {
+            View.VISIBLE
+        } else {
+            View.GONE
         }
     }
-}
-
-class CrimeSeriousAdapter(
-    private val crimes: List<Crime>
-) : RecyclerView.Adapter<SeriousCrimeHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SeriousCrimeHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = ListItemSeriousCrimeBinding.inflate(inflater, parent, false)
-        return SeriousCrimeHolder(binding)
-    }
-
-    override fun getItemCount(): Int = crimes.size
-
-    override fun onBindViewHolder(holder: SeriousCrimeHolder, position: Int) {
-        val crime = crimes[position]
-        holder.apply {
-            binding.crimeTitle.text = crime.title
-            binding.crimeDate.text = crime.data.toString()
-
-            binding.crimeSolved.visibility = if (crime.isSolved) {
-                View.VISIBLE
-            } else {
-                View.GONE
-            }
-        }
-        holder.bind(crime)
-    }
-
 }
 
 class CrimeListAdapter(
     private val crimes: List<Crime>
 ) : RecyclerView.Adapter<CrimeHolder>() {
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): CrimeHolder {
+    ) : CrimeHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
         return CrimeHolder(binding)
@@ -96,13 +49,9 @@ class CrimeListAdapter(
 
     override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
         val crime = crimes[position]
-        holder.apply {
-            binding.crimeTitle.text = crime.title
-            binding.crimeDate.text = crime.data.toString()
-        }
+
         holder.bind(crime)
     }
 
     override fun getItemCount() = crimes.size
-
 }
--- app/src/main/java/ru/stan/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/ru/stan/criminalintent/CrimeListViewModel.kt
@@ -1,22 +1,59 @@
 package ru.stan.criminalintent
 
 import androidx.lifecycle.ViewModel
-import java.util.Date
-import java.util.UUID
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+
+// 12.3 Delaying work: create a TAG to see how delaying work works
+private const val TAG = "CrimeListViewModel"
 
 class CrimeListViewModel : ViewModel() {
-    val crimes = mutableListOf<Crime>()
+
+    // 12.23 create a crimeRepository variable that grabs the getter out of the CrimeRepository
+    private val crimeRepository = CrimeRepository.get()
+
+    // 12.26 use the crimes variable to get the crimes from crimeRepository's getCrimes() function
+    //val crimes = crimeRepository.getCrimes()
+
+    // 12.28 Use StateFlow to cache database results
+    private val _crimes: MutableStateFlow<List<Crime>> = MutableStateFlow(emptyList())
+    val crimes: StateFlow<List<Crime>>
+        get() = _crimes.asStateFlow()
 
     init {
+        //(12.26) Log.d(TAG, "init starting")     // 12.3 Delaying work: Create a log that'll show that the previous init line ran
+        viewModelScope.launch {     // 12.2 Launch a coroutine
+            // 12.28
+            crimeRepository.getCrimes().collect {
+                _crimes.value = it
+
+            /*(12.26)Log.d(TAG, "coroutine launched")        // 12.3 Delaying work: create a log that indicates the viewModelScope.launch { ran
+            (12.26)crimes += loadCrimes()
+            (12.26)Log.d(TAG, "Loading crimes finished")   // 12.3 Delaying work: log that the coroutine is finished*/
+            }
+        }
+    }
+    /* 12.26 Get rid of all this code because we're using Flow and don't need this code
+    // 12.4 Define a suspending function called loadCrimes()
+    suspend fun loadCrimes(): List<Crime> {
+        /* 12.23 Remove code b/c now we have a database
+        val result = mutableListOf<Crime>()
+        delay(5000)// 12.3 Delaying work: delay for 5 seconds with delay(timeMillis:Long)
         for (i in 0 until 100) {
             val crime = Crime(
                 id = UUID.randomUUID(),
                 title = "Crime #$i",
-                data = Date(),
-                isSolved = i % 2 == 0,
-                requiresPolice = i % 2 == 0
+                date = Date(),
+                isSolved = i % 2 == 0
             )
-            crimes += crime
+            result += crime
         }
-    }
+        return result*/
+        // 12.23 Use the getCrimes() function to get all the crimes in the database
+        return crimeRepository.getCrimes()
+    }*/
 }
